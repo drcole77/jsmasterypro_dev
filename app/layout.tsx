124233ada07a97d30a9202ecfa7e616b154159a3
drcole77 <dustin@badgermountaindigital.com>
2025-06-23 16:22:34 -0700

    Forgot to save a file from the font update.

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.variable} ${spaceGrotesk.variable} antialiased`}
+        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
         {children}
       </body>
